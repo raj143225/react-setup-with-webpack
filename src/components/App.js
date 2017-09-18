4e75ddb437901b2bf0659a8b0b35772ea71f8aac
rajkumar <rajkumar.arisetty@mindfiresolutions.com>
2017-09-18 11:08:07 +0530

    Setup for reactjs with webpack applications

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+"use strict";
+
+import React, {PropTypes} from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import App from './components/App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'; // webpack can import CSS files too!
+import '../node_modules/toastr/build/toastr.min.css';
+
+render (
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
